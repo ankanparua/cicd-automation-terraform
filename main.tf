6603c4dcb0569d0bcdf6a5b010883f6621495e41
Ubuntu <ubuntu@ip-172-31-83-179.ec2.internal>
2026-03-20 20:40:19 +0000

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0ec10929233384c7f"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "CI-CD-Demo"
+  }
+}
